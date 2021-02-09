cdf900ce85d77060933beed6e038332a4d668b50
dnsantosuosso <78157706+dnsantosuosso@users.noreply.github.com>
2021-02-09 11:12:06 -0500

    Update game.py

--- game.py
+++ game.py
@@ -4,19 +4,6 @@
 Department of Computer Science,
 University of Toronto
 
-This code is provided solely for the personal and private use of
-students taking the CSC148 course at the University of Toronto.
-Copying for purposes other than this use is expressly prohibited.
-All forms of distribution of this code, whether as given or with
-any changes, are expressly prohibited.
-
-Authors: Diane Horton, David Liu, Mario Badr, Sophia Huynh, Misha Schwartz,
-and Jaisie Sin
-
-All of the files in this directory and all subdirectories are:
-Copyright (c) Diane Horton, David Liu, Mario Badr, Sophia Huynh,
-Misha Schwartz, and Jaisie Sin
-
 === Module Description ===
 
 This file contains the Game class, which is the main class for the
